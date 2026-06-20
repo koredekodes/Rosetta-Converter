602e5cc93507b9b4525fcf2fe3c273b772ba2063
Olukorede <krismacjob@gmail.com>
2026-06-20 14:14:59 +0100

    Added PyMuPDF vector-to-raster pipeline

--- rosetta.py
+++ rosetta.py
@@ -1,17 +1,31 @@
 import streamlit as st
 from PIL import Image
 import io
+import fitz
 import numpy as np
 
 st.title("Rosetta Stone")
 #UPLOAD THE IMAGE
-upload_file = st.file_uploader("Upload File", type = ["png","jpeg","jpg","webp"])
+upload_file = st.file_uploader("Upload File", type = ["png","jpeg","jpg","webp", "pdf"])
 #OPEN AND DISPLAY THE IMAGE
 if upload_file is not None:
-    img = Image.open(upload_file)
+    if upload_file.type == "application/pdf":
+        # Handle PDF file
+        doc = fitz.open(stream=upload_file.read(), filetype="pdf")
+        # Creates a slider if you want to convert multiple pages from the pdf
+        if doc.page_count > 1:
+            page_num = st.slider("Select Page to Convert", 1, doc.page_count,1)-1
+        else:
+            page_num = 0
+        page = doc[page_num]
+        
+        img = page.get_pixmap(dpi=150)
+        img = Image.frombytes("RGB", (img.width, img.height), img.samples)
+    else:
+        img = Image.open(upload_file)
     st.image(img, caption = "Original Image")
 
-    st.subheader("Conversion Subheader")
+    st.subheader("Conversion")
 
     #DROPDOWN DICTIONARY CHOICE MENU
     format_map = {#Choice on conversion type format [PIL format,file extension, MIME type]
